127fa603056dac38d3ccdd035b4c0234a4a3989f
d2000video-design <d2000video@gmail.com>
2026-05-08 09:35:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,21 @@
-const csvUrl = "https://docs.google.com/spreadsheets/d/e/2PACX-1vT_rkfrxq2yU3-LoMW5ud-l6AXRkZfVnPABf0mesQWt6QFG2fzZ0VVU-ixBRTHtt4JsHf1RvSPPPWrn/pub?output=csv";
+const apiUrl = "https://sheetdb.io/api/v1/qhnwkr61uiplq";
 
 async function carregarFeed() {
-
-  const resposta = await fetch(csvUrl);
-  const texto = await resposta.text();
-
-  const linhas = texto.split("\n").slice(1);
+  const resposta = await fetch(apiUrl);
+  const dados = await resposta.json();
 
   const feed = document.querySelector(".instagram-feed");
-
   feed.innerHTML = "";
 
-  linhas.forEach(linha => {
-
-    const colunas = linha.split(",");
-
-    const titulo = colunas[1];
-    const imagem = colunas[3];
-    const link = colunas[4];
-    const ativo = colunas[6];
-
-    if (ativo && ativo.trim() === "sim") {
-
-      const card = `
-        <a href="${link}" target="_blank" class="instagram-post">
-
-          <img src="${imagem}" alt="${titulo}">
-
+  dados.forEach(item => {
+    if (item.ativo && item.ativo.toLowerCase().trim() === "sim") {
+      feed.innerHTML += `
+        <a href="${item.link}" target="_blank" class="instagram-post">
+          <img src="${item.foto1}" alt="${item.titulo}">
         </a>
       `;
-
-      feed.innerHTML += card;
-
     }
-
   });
-
 }
 
 carregarFeed();
