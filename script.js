b42beca0319799a790f2ad41e546110e3b782484
d2000video-design <d2000video@gmail.com>
2026-05-08 11:05:20 -0300

    Update script.js

--- script.js
+++ script.js
@@ -19,3 +19,9 @@ async function carregarFeed() {
 }
 
 carregarFeed();
+const toggle = document.querySelector(".menu-toggle");
+const menu = document.querySelector(".mobile-menu");
+
+toggle.addEventListener("click", () => {
+  menu.classList.toggle("active");
+});
